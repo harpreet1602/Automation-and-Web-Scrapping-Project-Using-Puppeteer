410c2557ca2e4da066290fb162de6842bed09109
harpreet1602 <harpreets1602@gmail.com>
2021-05-09 02:00:55 +0530

    covid resources

--- script.js
+++ script.js
@@ -27,6 +27,14 @@ async function main() {
     await coping(tab);
     //await delay(5000);
     await vaccineCenters(tab);
+//    await covidResources(tab);
+await tab.goto("https://alpha2320.github.io/covid-19.io/");
+await covidResources(tab, "OxygenCylinders", 0);
+await covidResources(tab, "Hospitalbeds", 2);
+await covidResources(tab, "Onlineconsultation", 3);
+await covidResources(tab, "FoodSupply", 4);
+await covidResources(tab, "HomeQuarantine", 5);
+await covidResources(tab, "AmbulanceService", 6);
 }
 async function gotogoogle(tab) {
     await tab.goto("https://www.google.com/");
@@ -288,5 +296,44 @@ async function vaccineCenters(tab)
         if(i==names.length-1)
         fs.writeFileSync("VaccineCenters.json",JSON.stringify(vaccineCenters));
     }
+}
+async function covidResources(tab, boxname, i) {
+    await tab.waitForSelector(".text-2xl.font-semibold.items-center.py-6", { visible: true });
+    let boxes = await tab.$$(".text-2xl.font-semibold.items-center.py-6");
+    await tab.evaluate(function (ele) {
+        ele.click();
+    }, boxes[i]);
+    await tab.waitForSelector(".text-xl.font-semibold.my-2", { visible: true });
+    let names = await tab.$$(".text-xl.font-semibold.my-2");
+    let info = await tab.$$(".flex.space-x-2.text-gray-400.text-sm");
+    let verifiedAt = await tab.$$(".text-base.text-gray-400.font-semibold");
+    let k = 0;
+    let box = [];
+    for (let i = 0; i < names.length; i++) {
+        let name = await tab.evaluate(function (ele) {
+            return ele.innerText;
+        }, names[i]);
+        k = 2 * i;
+        let information1 = await tab.evaluate(function (ele) {
+            return ele.innerText;
+        }, info[k]);
+        let arr=information1.split("\n");
+        let info1=arr.join("");
+        let information2 = await tab.evaluate(function (ele) {
+            return ele.innerText;
+        }, info[k + 1]);
+        let verify = await tab.evaluate(function (ele) {
+            return ele.innerText;
+        }, verifiedAt[i]);
+        box.push({
+            "Name": name,
+            "Info1": info1,
+            "Info2": information2,
+            "Verified At": verify
+        });
+        if (i == names.length - 1)
+            fs.writeFileSync(boxname + ".json", JSON.stringify(box));
+    }
+    await tab.click('a[href="./index.html"]');
 }
 main();
